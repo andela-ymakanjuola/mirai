e7c7524e3e07037f492ac60de4e79adc84497236
yetunde.sanni@andela.com <yetunde.sanni@andela.com>
2016-11-25 18:50:01 +0100

    Initial commit

---
+++ scripts/logistic_regression.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression as lg
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import accuracy_score as acc
+
+train_data = pd.read_csv("../datasets/train.csv")
+# print(train_data.head(5))
+# print(train_data.describe())
+
+print train_data['Stage'].unique()
+
+# The columns we'll use to predict the target
+predictors = []
+
+# Initialize our algorithm class
+# alg = lg()
+# model = alg.fit()
