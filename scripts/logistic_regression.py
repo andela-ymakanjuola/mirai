6423bb1198e0024ed81e7bddcaac3798a865c60f
yetunde.sanni@andela.com <yetunde.sanni@andela.com>
2016-11-26 13:59:29 +0100

    split data and retrain model

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -7,14 +7,27 @@ from sklearn.metrics import accuracy_score as acc
 
 train_data = pd.read_csv("../datasets/train.csv")
 
-print train_data['Entry'].unique()
+from sklearn.cross_validation import train_test_split
+predictors = ['Score', '% Completed']
+train_target = train_data['Bootcamp']
+
+
+x_train, x_test, y_train, y_test = train_test_split(train_data[predictors], train_target) #split training data
 
 # The columns we'll use to predict the target
-predictors = []
+# train_predictors = train_data[predictors]
+# print train_predictors
 
 # Initialize our algorithm class
-# alg = lg()
-# model = alg.fit()
+alg = lg()
+model = alg.fit(x_train,y_train)
+
+# predictions
+predictions = model.predict(x_test)
+
+# accuracy_score
+accuracy_score = acc(predictions,y_test)
+print accuracy_score
 
 # correlation matrix
 correlations = train_data.corr()
