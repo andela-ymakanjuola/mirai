f8694786c6ad143d830458acd29c47a3d70cb5fc
yetunde.sanni@andela.com <yetunde.sanni@andela.com>
2016-11-27 15:01:10 +0100

    added test data and added predictions

--- scripts/ensembler_classifier.py
+++ scripts/ensembler_classifier.py
@@ -1,41 +1,60 @@
+from __future__ import division
 import pandas as pd
 import numpy as np
-import itertools
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from sklearn.cross_validation import cross_val_score
-from sklearn.metrics import accuracy_score as acc
-from mlxtend.classifier import EnsembleVoteClassifier
-from mlxtend.plotting import plot_decision_regions
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.decomposition import TruncatedSVD
+from sklearn.cross_validation import cross_val_score, train_test_split
+from sklearn.metrics import accuracy_score as acc,f1_score,roc_auc_score
 
-train_data = pd.read_csv("./datasets/train.csv")
+train_data = pd.read_csv("../datasets/train.csv")
+test = pd.read_csv("../datasets/test.csv")
+#train_data['score_v_completed']=train_data['Score']//train_data['% Completed']
+static_fields = []
+for c in train_data.columns:
+    if len(list(train_data[c].unique()))<=1:
+        static_fields.append(c)
 
-from sklearn.cross_validation import train_test_split
-predictors = ['Score', '% Completed']
-train_target = train_data['Bootcamp']
+static_fields
+train_data=train_data.drop(static_fields,axis=1)
+X=train_data.drop(['Joined Proctor on','Stage','% Score','Entry','Bootcamp'],axis=1)
+test_data = test.drop(static_fields,axis=1)
+test_data = test_data.drop(['Joined Proctor on','% Score', 'Bootcamp'],axis=1)
+y=train_data['Bootcamp']
+#x_train, x_test, y_train, y_test = train_test_split(X, y) #split training data
 
-abundant_class_data = train_data[train_data['Bootcamp'] == False]
-minority_class_data = train_data[train_data['Bootcamp'] == True]
-print abundant_class_data.shape
+# x_train=x_train[x_train['Score']>=92]
+x_train=X.drop(['S/N'],axis=1)
+# leak_test=x_test[x_test['Score']>=92]
+# leak_test=list(leak_test['S/N'])
+index=test_data['S/N']
+x_test=test_data.drop(['S/N'],axis=1)
+clf = LogisticRegression()
+grad = GradientBoostingClassifier(n_estimators=2000, max_depth=12, random_state=42)
+print 'x_train', x_train.info()
+print 'x_test',x_test.info()
+grad.fit(x_train,y)
 
-def generateRandomSamples(df):
-    samples = []
-    data = df
-    for i in xrange(55):
-        df_sample = data.sample(n=125)
-        samples.append(df_sample.append(minority_class_data))
-        data = data.drop(df_sample['S/N'].tolist(), axis=0, errors='ignore')
-    return samples
-random_samples = generateRandomSamples(abundant_class_data)
+enc = OneHotEncoder()
+enc.fit(grad.apply(x_train)[:, :, 0])
+decomp = TruncatedSVD(n_components=3, random_state=42)
+decomp.fit(x_train,y)
 
-classifiers = []
-weights = []
-for i in xrange(55):
-    classifiers.append(LogisticRegression(random_state=0))
-    weights.append(1)
+x_train = enc.transform(grad.apply(x_train)[:, :, 0])
+x_test = enc.transform(grad.apply(x_test)[:, :, 0])
+clf.fit(x_train,y)
 
-eclf = EnsembleVoteClassifier(clfs=classifiers, weights=weights, voting='soft')
+scores = cross_val_score(clf, x_train, y, cv=5, scoring='f1_weighted')
+print 'F1 score', scores
 
-train_sample_x = random_samples[1][predictors]
-train_sample_target = random_samples[1]['Bootcamp']
+predictions=clf.predict(x_test)
+
+accuracy_score = acc(test['Bootcamp'],predictions)
+print 'accuracy',accuracy_score
+
+accuracy_score = f1_score(test['Bootcamp'],predictions,average='weighted')
+print 'f1 on test', accuracy_score
+
+accuracy_score = roc_auc_score(test['Bootcamp'],predictions)
+print 'ROC', accuracy_score
